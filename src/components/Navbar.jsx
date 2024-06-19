c44a742fe60d373fb2a64567fea6aa905b13f00d
Saman <samangithoseinpour@gmail.com>
2024-06-19 10:24:00 +0330

    implement hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,15 @@ import { useRef } from 'react';
 import { useState } from 'react';
 import { Squash as Hamburger } from 'hamburger-react';
 import { AnimatePresence, motion } from 'framer-motion';
-// import { routes } from '../routes';
 
 import { navLists } from '../constants';
 
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway((ref) => setOpen(false));
+
   return (
     <header className="w-full py-12 sm:px-10 px-5 flex justify-around items-center">
       <nav className="flex w-full scrim-max-width">
@@ -15,13 +19,60 @@ const Navbar = () => {
           {navLists.map((nav) => (
             <div
               key={nav.id}
-              className="px-5 text-md cursor-pointer text-gray hover:text-white transition-all max-sm:hidden duration-700"
+              className="px-5 text-md cursor-pointer text-gray hover:text-white transition-all max-lg:hidden duration-700"
             >
               <a href={nav.href} className="max-sm">
                 {nav.label}
               </a>
             </div>
           ))}
+        </div>
+
+        {/* mobile navigation */}
+        <div ref={ref} className="lg:hidden fixed right-0 px-5 z-50">
+          <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="fixed left-0 shadow-4xl right-0 top-[6.5rem] p-5 pt-0 z-40"
+              >
+                <ul className="grid gap-2">
+                  {navLists.map((mobileNav, idx) => {
+                    return (
+                      <motion.li
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{
+                          type: 'spring',
+                          stiffness: 260,
+                          damping: 20,
+                          delay: 0.1 + idx / 10,
+                        }}
+                        key={mobileNav.id}
+                        className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
+                      >
+                        <a
+                          onClick={() => setOpen((prev) => !prev)}
+                          className={
+                            'flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950'
+                          }
+                          href={mobileNav.href}
+                        >
+                          <span className="flex gap-1 text-lg">
+                            {mobileNav.label}
+                          </span>
+                        </a>
+                      </motion.li>
+                    );
+                  })}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </nav>
     </header>
