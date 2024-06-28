f8c5052300a3cdb07776d1a446d0447881cef611
Saman <samangithoseinpour@gmail.com>
2024-06-28 09:10:14 +0330

    fix:youtube feed ui iss

--- src/components/YotubeFeed.jsx
+++ src/components/YotubeFeed.jsx
@@ -6,7 +6,7 @@ const YotubeFeed = ({ embedId }) => {
     <iframe
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-      className="rounded-sm duration-1000 mt-6 hover:opacity-80 flex-grow max-sm:flex-col"
+      className="rounded-sm duration-1000 mt-6 hover:opacity-80 max-2xl:flex-grow max-sm:flex-col"
       src={`https://www.youtube.com/embed/${embedId}`}
       width={450}
       height={300}
