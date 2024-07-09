caffe7fc2cf931ae43189f24897f7b806a44436f
Saman <samangithoseinpour@gmail.com>
2024-07-09 14:33:47 +0330

    ref: about&services pages

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,6 +1,5 @@
 import { HeadingText, ServicesCarousel } from '../components';
-import { serviceFeatures, stats } from '../constants/index';
-import { kengo4Img } from '../utils';
+import { stats } from '../constants/index';
 
 const ServicesPage = () => {
   return (
@@ -34,7 +33,7 @@ const ServicesPage = () => {
       {/* Ready to dive in */}
       {/* <div>
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="relative isolate overflow-hidden px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -47,12 +46,6 @@ const ServicesPage = () => {
                 fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
                 fillOpacity="0.7"
               />
-              <defs>
-                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stopColor="#7775D6" />
-                  <stop offset={1} stopColor="#E935C1" />
-                </radialGradient>
-              </defs>
             </svg>
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
@@ -93,7 +86,7 @@ const ServicesPage = () => {
       </div> */}
 
       {/* a better workflow */}
-      <div className="overflow-hidden">
+      {/* <div className="overflow-hidden">
         <div className="mx-auto max-w-7xl px-6">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div className="lg:pr-8 lg:pt-4">
@@ -135,7 +128,7 @@ const ServicesPage = () => {
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
