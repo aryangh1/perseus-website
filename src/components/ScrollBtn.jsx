bd7676eb27cc8b89f1d2845ab3464f691c721256
Saman <samangithoseinpour@gmail.com>
2024-07-15 18:44:08 +0330

    feat: smooth scroll to top

---
+++ src/components/ScrollBtn.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+const ScrollBtn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 2000);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={handleScrollToTop}
+          className="scroll-to-top fixed bottom-8 right-8 rounded-full transition duration-300 stroke-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="size-12"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollBtn;
